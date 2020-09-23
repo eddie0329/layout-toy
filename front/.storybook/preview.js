61f4ed390c1d39c3b2071cd1920fa96426596f17
Eddie <40482274+eddie0329@users.noreply.github.com>
2020-09-23 12:30:59 +0900

    :wrench: Change storybook config

--- front/.storybook/preview.js
+++ front/.storybook/preview.js
@@ -1,8 +1,8 @@
 import { configure } from "@storybook/vue";
+import Vuex from "vuex";
 import Vue from "vue";
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
 
 Vue.use(ElementUI);
-
-configure(require.context("../src/", true, /\.stories\.js$/), module);
+Vue.use(Vuex);
